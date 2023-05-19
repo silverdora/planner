278828531151d5a0904bc3a39c8558b61a52b33e
silverdora <dariagahova@mail.ru>
2023-05-18 22:26:28 -0300

    fastapi is added

--- api.py
+++ api.py
@@ -1,31 +1,28 @@
+from typing import List
+
+from fastapi import FastAPI
+
+import applications
 from models import Category, InputTask, Task
 
-categories = []
-
-tasks = []
+app = FastAPI()
 
 
+@app.get('/categories')
+def all_categories() -> List[Category]:
+    return applications.categories
+
+
+@app.post('/categories')
 def create_category(cat: str) -> Category:
-    new_category = Category(id=len(categories), name=cat)
-    categories.append(new_category)
-    return new_category
+    return applications.create_category(cat)
 
 
-def find_category(cat: int) -> Category:
-    for category in categories:
-        if cat == Category.id:
-            return category
-        raise ValueError
+@app.get('/tasks')
+def all_tasks() -> List[Task]:
+    return applications.tasks
 
 
-def create_task(params: InputTask) -> Task:
-    new_task = Task(name=params.text,
-                    creating_of_habit=params.create_habit,
-                    category=find_category(params.category),
-                    beginning_planned=params.start,
-                    finishing_planned=params.end,
-                    beginning_real=None,
-                    finishing_real=None,
-                    completed=False)
-    tasks.append(new_task)
-    return new_task
+@app.post('/tasks')
+def create_a_task(params: InputTask) -> Task:
+    return applications.create_task(params)
